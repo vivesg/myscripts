cc3b80d965284f5a9b525ac732c0d3e7d9ab4fba
German Vives <gvives@outlook.com>
2022-11-10 23:18:57 -0600

    Update ListVMSchedules.ps1

--- ListVMSchedules.ps1
+++ ListVMSchedules.ps1
@@ -15,15 +15,15 @@ $Schedules = Get-AzResource -ResourceType "microsoft.devtestlab/schedules"
 $Results = New-Object System.Collections.ArrayList
 
 foreach($schedule in $Schedules){
-    $currentSchedule = Get-AzResource -ResourceId $schedule.ResourceId  # Get the schedules
+    $currentSchedule = Get-AzResource -ResourceId $schedule.ResourceId  # Get the schedule properties
     $obj = New-Object -TypeName psobject
     $obj | Add-Member -MemberType NoteProperty -Name VMID -Value $currentSchedule.Properties.targetResourceId   # Get VMID
-    $obj | Add-Member -MemberType NoteProperty -Name Time -Value $currentSchedule.Properties.dailyRecurrence.time 
-    $obj | Add-Member -MemberType NoteProperty -Name TimeZone -Value $currentSchedule.Properties.timeZoneId
-    $obj | Add-Member -MemberType NoteProperty -Name IsEnabled -Value $currentSchedule.Properties.status
-    $name = $currentSchedule.Properties.targetResourceId.Split("/") 
+    $obj | Add-Member -MemberType NoteProperty -Name Time -Value $currentSchedule.Properties.dailyRecurrence.time  #GET THE SCHEDULE Time 
+    $obj | Add-Member -MemberType NoteProperty -Name TimeZone -Value $currentSchedule.Properties.timeZoneId # get the timezone
+    $obj | Add-Member -MemberType NoteProperty -Name IsEnabled -Value $currentSchedule.Properties.status  # get if the schedule is enabled or disabled
+    $name = $currentSchedule.Properties.targetResourceId.Split("/")   # split the resource id to get the RG and Name
 
-    $obj | Add-Member -MemberType NoteProperty -Name RGName -Value $name[-5]
+    $obj | Add-Member -MemberType NoteProperty -Name RGName -Value $name[-5]  
     $obj | Add-Member -MemberType NoteProperty -Name VMName -Value $name[-1]
     [void]$results.Add($obj) 
 }
