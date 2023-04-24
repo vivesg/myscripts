c92270f2947e719100219226412d7b9caa290c2c
German Vives <gvives@outlook.com>
2023-04-24 17:42:38 -0600

    Update VMAPISpeedup.ps1

--- VMAPISpeedup.ps1
+++ VMAPISpeedup.ps1
@@ -1,6 +1,13 @@
+#PLEASE USE AS EDUCATIONAL PURPOSES
+#Take this as it is non warranty
+
 $SUBID = "FFFF-FFFF-FFFF-FFF-FFF"
 Connect-AzAccount 
 Set-AzContext -Subscription $SUBID
+
+#please check https://learn.microsoft.com/en-us/rest/api/compute/virtual-machines/list-all?tabs=HTTP#code-try-0
+# this code does not handle nextLink iteration 
+# if you have more vm pages  (a lot of VMs), please implement the nextLink call  
 $ENDPOINT = "https://management.azure.com/subscriptions/" + $SUBID + "/providers/Microsoft.Compute/virtualMachines?api-version=2022-11-01&statusOnly=true"
 $token = (Get-AzAccessToken).Token
 
@@ -20,4 +27,6 @@ Measure-Command {
 }
 
 Write-Host "Trying Powershell AZ Module"
-Measure-Command { Get-AzVM -Status }
+Measure-Command { 
+Get-AzVM -Status 
+}
